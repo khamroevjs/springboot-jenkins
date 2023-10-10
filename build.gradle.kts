30076332c79f129a68efa6eb218ff3466c36c3d9
Jushkinbek Khamroev <khamroevjs@gmail.com>
2023-10-10 14:53:19 +0500

    fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.3"
 	kotlin("jvm") version "1.8.22"
 	kotlin("plugin.spring") version "1.8.22"
+	id("org.sonarqube") version "4.4.1.3373"
 	war
 }
 
